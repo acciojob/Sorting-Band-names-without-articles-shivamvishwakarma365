7f11843ba404263713fcf52b38c25168b1f2251e
shivamvishwakarma365 <78745548+shivamvishwakarma365@users.noreply.github.com>
2023-06-23 23:00:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,15 @@
-const bandNames = [
-  'The Rolling Stones',
-  'Pink Floyd',
-  'Led Zeppelin',
-  'The Beatles',
-  'Nirvana',
-  'Red Hot Chili Peppers',
-  'The Who',
-];
+const bands = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
-function stripArticle(name) {
-  const articles = ['a', 'an', 'the'];
-  const words = name.split(' ');
-  if (articles.includes(words[0].toLowerCase())) {
-    words.shift();
-  }
-  return words.join(' ');
+function stripArticles(bandName) {
+  return bandName.replace(/^(a |an |the )/i, '').trim();
 }
 
-const sortedBandNames = bandNames.sort((a, b) => {
-  const bandA = stripArticle(a);
-  const bandB = stripArticle(b);
-  return bandA.localeCompare(bandB);
-});
+const sortedBands = bands.sort((a, b) => stripArticles(a) > stripArticles(b) ? 1 : -1);
 
-const ul = document.querySelector('#bands');
+const bandsList = document.querySelector('ul');
 
-sortedBandNames.forEach((band) => {
-  const li = document.createElement('li');
-  li.textContent = band;
-  ul.appendChild(li);
+sortedBands.forEach((band) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = band;
+  bandsList.appendChild(listItem);
 });
