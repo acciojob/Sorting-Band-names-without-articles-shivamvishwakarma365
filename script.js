2fe08b40cd0cabf9b9c9e11a6020777501e2445b
shivamvishwakarma365 <78745548+shivamvishwakarma365@users.noreply.github.com>
2023-06-23 22:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 //your code here
 
+const bandNames = [
+  'The Rolling Stones',
+  'Pink Floyd',
+  'Led Zeppelin',
+  'The Beatles',
+  'Nirvana',
+  'Red Hot Chili Peppers',
+  'The Who',
+];
+
+function stripArticle(name) {
+  const articles = ['a', 'an', 'the'];
+  const words = name.split(' ');
+  if (articles.includes(words[0].toLowerCase())) {
+    words.shift();
+  }
+  return words.join(' ');
+}
+
+const sortedBandNames = bandNames.sort((a, b) => {
+  const bandA = stripArticle(a);
+  const bandB = stripArticle(b);
+  return bandA.localeCompare(bandB);
+});
+
+const ul = document.querySelector('#bands');
+
+sortedBandNames.forEach((band) => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
